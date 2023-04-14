8476304f3b2b8fe2403b60278157e427364e6fff
alejandro <alejandrober96@gmail.com>
2023-04-14 09:48:38 -0500

    likes bug

--- src/modules/giveLikes.js
+++ src/modules/giveLikes.js
@@ -7,14 +7,14 @@ export default async () => {
   const likes = document.querySelectorAll('.likes');
   likes.forEach((like) => {
     like.addEventListener('click', () => {
-      like.style.boxShadow = '0px 0px 3px 3px red';
+      like.style.boxShadow = '0px 0px 3px 3px white';
       like.style.backgroundImage = `url('${likeImg}')`;
       like.classList.add('liked');
       setTimeout(() => {
         like.style.boxShadow = '0px 0px';
       }, 3000);
       if (checkLocalStorage(like.parentNode.parentNode.id) === false) {
-        setLikes(like.previousSibling.textContent);
+        setLikes(like.parentNode.previousSibling.textContent);
       }
       setLocalStorage();
     });
